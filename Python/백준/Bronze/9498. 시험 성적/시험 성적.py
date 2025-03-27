f235570a3d625b5fb9ab3190742e098c87acd19e
Baeck Dong Jae <161856415+VectorSophie@users.noreply.github.com>
2025-03-27 16:03:11 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+enter = int(input())
+if enter<= 100 and enter >= 90:
+    print('A')
+elif enter<=89 and enter >= 80:
+    print('B')
+elif enter<=79 and enter >= 70:
+    print('C')
+elif enter<=69 and enter >= 60:
+    print('D')
+else:
+    print('F')
