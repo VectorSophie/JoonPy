d7f0475bb935e2758e0d8e70559e01e4d4e565de
Baeck Dong Jae <161856415+VectorSophie@users.noreply.github.com>
2025-05-21 23:49:26 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,13 @@
+while True:
+    line = input()
+    if line == "# 0 0":
+        break
+
+    name, age, weight = line.split()
+    age = int(age)
+    weight = int(weight)
+
+    if age > 17 or weight >= 80:
+        print(f"{name} Senior")
+    else:
+        print(f"{name} Junior")
